931b1e0e347cda51abbe949cd8dec8028d3e6c8f
apantol <apantol@hotmail.com>
2016-10-15 16:13:33 +0200

    Add files via upload

--- sine_gen.m
+++ sine_gen.m
@@ -1,17 +1,24 @@
-clc;
-clear;
 
-fid = fopen('sine_in.txt', 'w');
+generate = 0;
 
 bn = 12;
 amp = (2^bn/2)-1;
 f  = 1e3;
 fs = 20e3;
 
-t = 0:(1/fs):2047*(1/fs);
+if(generate)
+    clc;
+    clear;
 
-
-y = amp*sin(2*pi*f*t);
-y = round(y);
-
-fprintf(fid, '%d\n', y);
+    fid = fopen('sine_in.txt', 'w');
+    t = 0:(1/fs):2047*(1/fs);
+    y = amp*sin(2*pi*f*t);
+    y = round(y);
+    fprintf(fid, '%d\n', y);
+else
+     fid = fopen('sine_out.txt', 'r');
+     sine_out = fscanf(fid, '%d\n');
+     
+     sine_in_fft = abs(fft(y, 1024));
+     sine_out_fft = abs(fft(sine_out,1024));
+end
